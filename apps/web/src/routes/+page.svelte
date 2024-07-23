185e3d96a540862e2a6cb9711d971542ee850ed8
Dennie de Lange <621098+tkvw@users.noreply.github.com>
2024-07-23 15:00:30 +0200

    wip

--- apps/web/src/routes/+page.svelte
+++ apps/web/src/routes/+page.svelte
@@ -1,7 +1,10 @@
 <script lang="ts">
+	import { add } from '@repo/math/add';
 	import { MyCounterButton } from '@repo/ui';
 </script>
 
 <h1>Web</h1>
 <MyCounterButton />
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+
+<div>{add(1, 2)}</div>
